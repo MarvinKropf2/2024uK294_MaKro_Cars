f672219de31815edc6714c17f008e1772268bda0
Marvin Kropf <143713628+MarvinKropf2@users.noreply.github.com>
2024-05-29 16:02:07 +0200

    Logout Button wiederhergestellt

--- my-app/src/assets/Components/Molecules/Navbar.tsx
+++ my-app/src/assets/Components/Molecules/Navbar.tsx
@@ -2,17 +2,29 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { Link } from 'react-router-dom';
-import HomeButton from '../Atom/HomeButton'; 
+import { Button } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Navbar() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('token'); 
+    navigate('/'); 
+  };
+
   return (
     <AppBar position="static" className="navbar">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Car App
         </Typography>
-        <HomeButton /> {}
+        <Button variant="contained" color="primary" component={Link} to="/car">
+          Home
+        </Button>
+        <Button variant="contained" color="secondary" onClick={handleLogout} style={{ marginLeft: '10px' }}>
+          Logout
+        </Button>
       </Toolbar>
     </AppBar>
   );
